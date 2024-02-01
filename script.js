a975a5c526d1dd8988155e8ccc77da9bac88c7ee
August <auha0001@stud.kea.dk>
2024-02-01 08:59:17 +0100

    övningar i javascript 1-3 onsdag 31/1-24

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("stackars katt");
+const beer = document.querySelector("img[alt=beer");
+const snaps = document.querySelector("img[alt=snaps");
+const cola = document.querySelector("img[alt=cola");
+const fanta = document.querySelector("img[alt=fanta");
+
+beer.addEventListener("click", tjek);
+
+function tjek() {
+  console.log(this.alt);
+  if (this.alt == "beer" || this.alt == "snaps") {
+    console.log("innehåller alkohol");
+  } else {
+    console.log("alkoholfri");
+  }
+}
